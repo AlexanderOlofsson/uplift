40ea8ba57910177f495b95c9ba3de40bb8930afa
muab01 <mustafasis1995@gmail.com>
2024-11-04 23:43:32 +0100

    home/pages

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,22 @@
+function Header() {
+    return (
+        <div className="container">
+            <section className="sticky">
+                <nav>
+                    <div className="logo">
+                        <a href="#">Uplift</a>
+                    </div>
+                    <div className="links">
+                        <a href="#">Skapa Konto</a>
+                        <a href="#">Logga In</a>
+                    </div>
+                </nav>
+                <div className="header">
+                    <h1>Uplift</h1>
+                </div>
+            </section>
+        </div>
+    );
+}
+
+export default Header;
