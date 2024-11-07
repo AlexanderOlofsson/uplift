0d1a5bd60c12923f8848654c6080dad9e139acd9
muab01 <mustafasis1995@gmail.com>
2024-11-07 23:00:59 +0100

    Added PhysicalPage + Page.css

---
+++ frontend/src/pages/PhysicalPage.jsx
@@ -0,0 +1,79 @@
+import { motion } from 'framer-motion';
+import '../styles/page.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import image3 from '../assets/images/image3.jpg';
+import { useNavigate } from 'react-router-dom';
+
+function PhysicalPage() {
+const navigate = useNavigate()
+
+const TextAppear = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0, transition: { duration: 1, ease: 'easeOut' } },
+  };
+
+  const logoEffect = {
+    hidden: { opacity: 0, y: -20 },
+    visible: { opacity: 1, y: 0, transition: { duration: 0.8, ease: 'easeOut' } },
+  };
+
+  const imageHoverEffect = {
+    hidden: { opacity: 0, scale: 0.95 },
+    visible: { opacity: 1, scale: 1, transition: { duration: 1, delay: 0.4, ease: 'easeOut' } },
+  };
+
+  const textHoverEffect = {
+    hidden: { opacity: 0, y: 10 },
+    visible: { opacity: 1, y: 0, transition: { duration: 0.8, delay: 0.6, ease: 'easeOut' } },
+  };
+
+  return (
+    <motion.div
+      className="container"
+      initial="hidden"
+      animate="visible"
+      variants={TextAppear}
+    >
+      <header>
+        <motion.div variants={logoEffect}>
+          <FontAwesomeIcon icon={faArrowLeft} className="back-arrow" onClick={() => navigate('/welcome')} />
+        </motion.div>
+        <motion.h1 className="logo" variants={logoEffect}>
+          Uplift
+        </motion.h1>
+      </header>
+
+      <main>
+        <motion.img
+          src={image3}
+          alt="Physical Task"
+          className="center-image"
+          variants={imageHoverEffect}
+        />
+        
+        <motion.h2 className="task-title" variants={textHoverEffect}>
+        Today&apos;s Physical Task
+        </motion.h2>
+
+        
+        <motion.div className="reminder-text" variants={textHoverEffect}>
+          <p>Don&apos;t forget to complete your daily task before midnight!</p>
+        </motion.div>
+        
+        <motion.div className="task-container" variants={textHoverEffect}>
+          {/*Vi har en tom container tills datan från vår Postgres är färdig */}
+        </motion.div>
+      </main>
+
+      <footer>
+        <motion.h2 className="logo" variants={logoEffect}>
+          Uplift
+        </motion.h2>
+        <motion.p variants={textHoverEffect}>&copy; 2024 Uplift. All rights reserved.</motion.p>
+      </footer>
+    </motion.div>
+  )
+}
+
+export default PhysicalPage;
